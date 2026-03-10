f2caafde216b1a8209391382ce72b14ea3a167f9
JIB <glitter.gamer011@gmail.com>
2026-03-10 20:16:19 +0700

    fix cors and api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,14 +4,3 @@ export const getStock = async () => {
   const res = await fetch(`${API_URL}/api/stock`);
   return res.json();
 };
-
-export const addStock = async (data) => {
-  const res = await fetch(`${API_URL}/api/stock`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  });
-  return res.json();
-};
