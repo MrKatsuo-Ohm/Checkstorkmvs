3e8e17ef44ea1a1a17f2696ec6653fc8fc51f692
JIB <glitter.gamer011@gmail.com>
2026-03-10 20:24:11 +0700

    fix api connection

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://checkstorkmvs-1.onrender.com/api/stock";
+const API_URL = "https://checkstorkmvs-1.onrender.com";
 
 export const getStock = async () => {
   const res = await fetch(`${API_URL}/api/stock`);
