c743173d3d92c5237615b678f35f855e7e6fb877
JIB <glitter.gamer011@gmail.com>
2026-03-10 20:08:35 +0700

    update cors and add gitignore

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://checkstorkmvs.vercel.app"
+  ]
+}));
 app.use(express.json());
 
 // Routes
