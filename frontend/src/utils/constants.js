a31f994317dffa62cb7a541fac83881a85cf847a
MrKatsuo <glitter.gamer011@gmail.com>
2026-03-14 14:12:38 +0700

    fixname

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,40 +1,40 @@
 export const categories = {
-  hardware: {
+    ฮาร์ดแวร์: {
     name: 'ฮาร์ดแวร์',
     icon: 'Cpu',
     subcategories: ['CPU', 'GPU', 'RAM PC', 'RAM Notebook', 'Motherboard', 'PSU', 'Case', 'SSD M.2', 'SSD SATA', 'SSD External', 'HDD Internal', 'HDD External', 'Cooling', 'Case Fan', 'Optical Drive', 'Sound Card']
   },
-  accessories: {
+  อุปกรณ์เสริม: {
     name: 'อุปกรณ์เสริม',
     icon: 'Mouse',
     subcategories: ['เมาส์', 'คีย์บอร์ด', 'หูฟัง/หูฟังไร้สาย', 'ลำโพง', 'ลำโพง Bluetooth', 'เว็บแคม', 'ไมโครโฟน/Content Creator', 'แผ่นรองเมาส์', 'Hub USB', 'Adapter Notebook', 'Adapter Smartphone', 'Cable', 'Card Reader', 'Connector/Hub', 'Controller', 'Cooler Pad', 'Joystick', 'Monitor Arm', 'Power Bank']
   },
-  monitors: {
+  จอมอนิเตอร์: {
     name: 'จอมอนิเตอร์',
     icon: 'Monitor',
     subcategories: ['จอ 15-22 นิ้ว', 'จอ 23-24 นิ้ว', 'จอ 27 นิ้ว', 'จอ 31+ นิ้ว', 'จอโชว์']
   },
-  networking: {
+  อุปกรณ์เครือข่าย: {
     name: 'อุปกรณ์เครือข่าย',
     icon: 'Wifi',
     subcategories: ['Router', 'Mobile Router', 'Switch', 'Access Point', 'สาย LAN', 'Network Card', 'Wireless USB', 'IP Camera']
   },
-  software: {
+  ซอฟต์แวร์: {
     name: 'ซอฟต์แวร์',
     icon: 'Code',
     subcategories: ['Windows License', 'Office License', 'Antivirus', 'อื่นๆ']
   },
-  storage: {
+  อุปกรณ์จัดเก็บ: {
     name: 'อุปกรณ์จัดเก็บ',
     icon: 'HardDrive',
     subcategories: ['Flash Drive', 'Memory Card', 'Enclosure']
   },
-  notebook: {
+  โน้ตบุ๊ก: {
     name: 'โน้ตบุ๊ก',
     icon: 'Laptop',
     subcategories: ['Notebook ทั่วไป', 'Notebook Gaming', 'Notebook Ultrathin', 'Film Notebook', 'Bag Notebook']
   },
-  peripherals: {
+  อุปกรณ์ต่อพ่วง: {
     name: 'อุปกรณ์ต่อพ่วง',
     icon: 'Layers',
     subcategories: ['Plug/Surge Protector', 'Cleaning',]
@@ -44,7 +44,7 @@ export const categories = {
     icon: 'Printer',
     subcategories: ['Printer', 'Toner/Ink', 'Scanner']
   },
-  misc : {
+  อุปกรณ์อื่นๆ : {
     name: 'อุปกรณ์อื่นๆ',
     icon: 'MoreHorizontal ',
     subcategories: ['อื่นๆ']
