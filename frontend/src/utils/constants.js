4d3c3aca1cd5621466bab80a0e4ba6bf0ed173af
MrKatsuo <glitter.gamer011@gmail.com>
2026-03-22 18:28:54 +0700

    fixcatnotebook

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,7 +2,7 @@ export const categories = {
   hardware: {
     name: 'ฮาร์ดแวร์',
     icon: 'Cpu',
-    subcategories: ['CPU', 'GPU', 'RAM PC', 'RAM Notebook', 'Motherboard', 'PSU', 'Case', 'SSD M.2', 'SSD SATA', 'SSD External', 'HDD Internal', 'HDD External', 'Cooling', 'Case Fan', 'Optical Drive', 'Sound Card']
+    subcategories: ['CPU', 'GPU', 'RAM PC', 'RAM Notebook', 'Motherboard', 'PSU', 'Case', 'SSD M.2', 'SSD SATA', 'HDD Internal', 'Cooling', 'Case Fan', 'Optical Drive', 'Sound Card']
   },
   accessories: {
     name: 'อุปกรณ์เสริม',
@@ -32,7 +32,7 @@ export const categories = {
   notebook: {
     name: 'โน้ตบุ๊ก',
     icon: 'Laptop',
-    subcategories: ['Notebook ทั่วไป', 'Notebook Gaming', 'Notebook Ultrathin', 'Film Notebook', 'Bag Notebook']
+    subcategories: ['Notebook ทั่วไป', 'Notebook Gaming', 'Notebook Ultrathin', 'Film Notebook', 'Bag Notebook', 'Giftbox Notebook']
   },
   peripherals: {
     name: 'อุปกรณ์ต่อพ่วง',
